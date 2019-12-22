c949d502337ea66e267fcd68a6a4fb33da2e2d32
fyui001 <yun.cod@gmail.com>
2019-12-23 04:54:00 +0900

    VuexModuleの大方完成

--- src/store/ImasChikoCheckVuexModule.ts
+++ src/store/ImasChikoCheckVuexModule.ts
@@ -1,8 +1,7 @@
-import { Mutation, Action, VuexModule, getModule, Module } from "vuex-module-decorators"
+import {Mutation, Action, VuexModule, getModule, Module} from "vuex-module-decorators"
 import store from './store'
 import VoiceActtorName from '../modules/VoiceActorName'
 import axios from 'axios'
-
 
 
 @Module({dynamic: true, store, name: 'ShikoCheckList-Store'})
@@ -10,6 +9,8 @@ export class ImasShikoCheckVuexModuleClass extends VuexModule {
 
   imasVoiceActors: VoiceActtorName[] = []
   shikoCheckList: string[] = []
+  isFetching = false
+  usrToken = ''
 
   @Mutation
   public setVoiceActors(param: VoiceActtorName[]) {
@@ -21,13 +22,22 @@ export class ImasShikoCheckVuexModuleClass extends VuexModule {
     this.shikoCheckList = param
   }
 
+  @Mutation
+  public setUrlToken(param: string) {
+    this.usrToken = param
+  }
+
   @Action({})
   public getVoiceActors() {
     axios({
       method: 'GET',
       url: 'https://app.mogamin.net/api/shiko/get'
     }).then((result: any) => {
-      this.setVoiceActors(result.data.voiceActors)
+
+        this.setVoiceActors(result.data.voiceActors)
+
+    }).catch(() => {
+
     })
   }
 
@@ -40,11 +50,49 @@ export class ImasShikoCheckVuexModuleClass extends VuexModule {
         usrToken
       }
     }).then((result: any) => {
-      const voiceActors: VoiceActtorName[] = result.data.voiceActors
-      const shikoLists: string[] = JSON.parse(result.data.shikoList[0].shiko_list)
-      console.log(shikoLists)
-      this.setVoiceActors(voiceActors)
-      this.setShikoCheckList(shikoLists)
+      const responce = result.data
+      if (responce.status) {
+
+        const voiceActors: VoiceActtorName[] = result.data.voiceActors
+        this.setVoiceActors(voiceActors)
+        this.setShikoCheckList(JSON.parse(responce.shikoList[0].shiko_list))
+
+      }
+    })
+  }
+
+  @Action({})
+  public createVoiceActorShikoList(shikoCheckList: string): void {
+    axios({
+      method: 'POST',
+      url: 'https://app.mogamin.net/api/shiko/create',
+      params: {
+        'shikoList': shikoCheckList
+      }
+    }).then((result: any) => {
+      const responce = result.data
+
+      if (responce.status) {
+        this.setUrlToken(responce.urlToken)
+      }
+
+    })
+  }
+
+  @Action({})
+  public updateVoiceActorShikoList(usrToken: string) {
+    const json = JSON.stringify(ImasShikoCheckListVuexModule.shikoCheckList)
+    axios({
+      method:'POST',
+      url: 'https://app.mogamin.net/api/shiko/update',
+      params: {
+        'shikoList': json,
+        'usrToken': usrToken
+      }
+    }).then((result) => {
+
+    }).catch(() => {
+
     })
   }
 
