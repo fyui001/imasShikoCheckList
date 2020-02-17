f2575666278b2705c8f95b1701beac2c908031db
fyui001 <yun.cod@gmail.com>
2020-02-17 12:43:38 +0900

    APIから持ってくる値の方を変えた

---
+++ src/modules/ImasCharacters.ts
@@ -0,0 +1,12 @@
+export default interface ImasCharacters {
+  id: number;
+  name: string;
+  nameKana: string;
+  age: number;
+  bust: number;
+  hip: number;
+  waist: number;
+  color: string;
+  voiceActor: string;
+  title: string;
+}
