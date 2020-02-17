f7e24e8b8765e5f1d22289a2c45068359087a01f
fyui001 <yun.cod@gmail.com>
2020-02-17 14:31:25 +0900

    keyをDBのカラム名に合わせた

--- src/modules/ImasCharacters.ts
+++ src/modules/ImasCharacters.ts
@@ -1,12 +1,14 @@
 export default interface ImasCharacters {
   id: number;
   name: string;
-  nameKana: string;
+  // eslint-disable-next-line camelcase
+  name_kana: string;
   age: number;
   bust: number;
   hip: number;
   waist: number;
   color: string;
-  voiceActor: string;
+  // eslint-disable-next-line camelcase
+  voice_actor: string;
   title: string;
 }
